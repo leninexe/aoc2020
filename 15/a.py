97cd3a94273d15ae001daf3020adb643ac08070d
Markus Wolf <mawiwo@a1.net>
2020-12-15 10:05:32 +0100

    Solution for day 15

---
+++ 15/a.py
@@ -0,0 +1,28 @@
+# Read Input
+f = open("input.txt", "r")
+
+numbers = {}
+splitter = f.readline().strip().split(",")
+
+for i in range(len(splitter)):
+	numbers[int(splitter[i])] = i + 1
+
+mostRecent = int(splitter[len(splitter) - 1])
+previousTurn = 0
+
+for turn in range(1, 2021):
+
+	if turn <= len(splitter):
+		continue
+
+	if previousTurn == 0:
+		mostRecent = 0
+		previousTurn = numbers.get(mostRecent, 0)
+		numbers[mostRecent] = turn
+		continue
+
+	mostRecent = numbers[mostRecent] - previousTurn
+	previousTurn = numbers.get(mostRecent, 0)
+	numbers[mostRecent] = turn
+
+print(mostRecent)
